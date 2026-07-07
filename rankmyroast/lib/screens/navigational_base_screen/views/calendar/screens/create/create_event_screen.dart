84711e5f1392a44cef6d318ab4fa1cd7ce5026f2
James Mata <jamesmata556@gmail.com>
2026-07-07 11:00:55 -0400

    more stuff

--- rankmyroast/lib/screens/navigational_base_screen/views/calendar/screens/create/create_event_screen.dart
+++ rankmyroast/lib/screens/navigational_base_screen/views/calendar/screens/create/create_event_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rankmyroast/classes/extra/create_event_extra.dart';
+import 'package:rankmyroast/classes/modals/group.dart';
+import 'package:rankmyroast/classes/modals/recipe.dart';
 import 'package:rankmyroast/classes/modals/schedule.dart';
+import 'package:rankmyroast/services/supabase_helper.dart';
 
 class CreateEventScreen extends StatefulWidget {
   final CreateEventExtra? extra;
@@ -16,6 +19,15 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
   late final String _labelText;
   late final Schedule? _eventToEdit;
 
+  final DateTime? _selectedDate = null;
+  final Recipe? _selectedRecipe = null;
+  final Group? _selectedGroup = null;
+
+  Future<List<Recipe>>? _recipesFuture;
+  late final Future<List<Group>?> _groupsFuture;
+
+  final TextEditingController _notesController = TextEditingController();
+
   @override
   void initState() {
     if (widget.extra != null) {
@@ -25,7 +37,7 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
       _labelText = "Create Event";
       _eventToEdit = null;
     }
-
+    _groupsFuture = _getGroups();
     super.initState();
   }
 
@@ -58,6 +70,21 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
           ],
         ],
       ),
+      body: Padding(padding: EdgeInsets.all(8), child: Column(children: [])),
     );
+  }
+
+  Future<List<Group>?> _getGroups() {
+    return SupabaseHelper.groups.getGroupsForUser();
+  }
+
+  Future<List<Recipe>?> _getRecipes(final String groupId) {
+    return SupabaseHelper.recipe.getRecipesByGroupId(groupId);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _notesController.dispose();
   }
 }
